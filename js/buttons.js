8b82971b4307e0f044e547a48cb82efcbbb6e115
Roman <romanaaav@gmail.com>
2020-05-26 00:30:37 -0300

    Finished mobile navigation
    Finished JQuery mobile navigation.
    Made some aesthetic tweaks in media queries.
    Migrated all Javascript from "script.js" to "buttons.js"

--- js/buttons.js
+++ js/buttons.js
@@ -1,36 +1,41 @@
-const homeLink = document.getElementById("homeButton").addEventListener("click",llevarAHome)
-const aboutLink = document.getElementById("aboutButton").addEventListener("click",llevarAAbout);
-const portfolioLink = document.getElementById("portfolioButton").addEventListener("click",llevarAPortfolio);
-const contactLink = document.getElementById("contactButton").addEventListener("click",llevarAContacto);
-const contactButton = document.getElementsByClassName("btn-ghost")[0].addEventListener("click",llevarAContacto);
-const portfolioButton = document.getElementsByClassName("btn-full")[0].addEventListener("click",llevarAPortfolio);
+const homeLink = document.getElementById("homeButton").addEventListener("click", llevarAHome)
+const aboutLink = document.getElementById("aboutButton").addEventListener("click", llevarAAbout);
+const portfolioLink = document.getElementById("portfolioButton").addEventListener("click", llevarAPortfolio);
+const contactLink = document.getElementById("contactButton").addEventListener("click", llevarAContacto);
+const contactButton = document.getElementsByClassName("btn-ghost")[0].addEventListener("click", llevarAContacto);
+const portfolioButton = document.getElementsByClassName("btn-full")[0].addEventListener("click", llevarAPortfolio);
 
-function llevarAHome(e){
+function llevarAHome(e) {
     e.preventDefault();
-    scrollEffect('#home',1000);
+    mobileNavReset();
+    scrollEffect('#home', 1000);
 }
 
-function llevarAAbout(e){
+function llevarAAbout(e) {
     e.preventDefault();
-    scrollEffect('#about',1000);
+    mobileNavReset();
+
+    scrollEffect('#about', 1000);
 }
 
-function llevarAPortfolio(e){
+function llevarAPortfolio(e) {
     e.preventDefault();
-    scrollEffect('#portfolio',1000);
+    mobileNavReset();
+    scrollEffect('#portfolio', 1000);
 
 }
 
-function llevarAContacto(e){
+function llevarAContacto(e) {
     e.preventDefault();
-    scrollEffect('#contact',1000);
+    mobileNavReset();
+    scrollEffect('#contact', 1000);
 }
 
-function scrollEffect(target,duration){
+function scrollEffect(target, duration) {
     var target = document.querySelector(target);
     var offset = 0;
 
-    if(target != document.querySelector('#home')) {
+    if (target != document.querySelector('#home')) {
         offset = -75;
     }
 
@@ -41,16 +46,16 @@ function scrollEffect(target,duration){
 
     var startTime = null;
 
-    function scrollAnimation(currentTime){
-        if (startTime === null){
+    function scrollAnimation(currentTime) {
+        if (startTime === null) {
             startTime = currentTime;
         }
         var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed,startPosition,targetPosition,duration);
+        var run = ease(timeElapsed, startPosition, targetPosition, duration);
 
-        window.scrollTo(startPosition,run);
+        window.scrollTo(startPosition, run);
 
-        if (timeElapsed < duration){
+        if (timeElapsed < duration) {
             requestAnimationFrame(scrollAnimation);
         }
     }
@@ -59,9 +64,64 @@ function scrollEffect(target,duration){
     function ease(t, b, c, d) {
         t /= d;
         t--;
-        return c*(t*t*t + 1) + b;
+        return c * (t * t * t + 1) + b;
     };
     window.requestAnimationFrame(scrollAnimation);
 
-    console.log("Posicion inicial: " +startPosition+ ". Posicion Final: " + targetPosition + ". Distancia: " + distance )
+    console.log("Posicion inicial: " + startPosition + ". Posicion Final: " + targetPosition + ". Distancia: " + distance)
 }
+
+function mobileNavReset() {
+    let nav = $('.nav-options ul');
+    let icon = $('.--js-nav-icon i');
+
+    if($(window).width() > 768) {
+       return; 
+    }
+
+    nav.slideToggle(200);
+    /* switch from hamburger to cross icon */
+    if (icon.hasClass('ion-navicon-round')) {
+        nav.css({
+            "display": "block"
+        })
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+    } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+    }
+}
+
+/* Mobile Navigation */
+$('.--js-nav-icon').click(function () {
+    let nav = $('.nav-options ul');
+    let icon = $('.--js-nav-icon i');
+
+    nav.slideToggle(200);
+    /* switch from hamburger to cross icon */
+    if (icon.hasClass('ion-navicon-round')) {
+        nav.css({
+            "display": "block"
+        })
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+    } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+    }
+});
+
+$(document).ready(function () {
+
+    $('.about-section').waypoint(function (direction) {
+        if (direction === 'down') {
+            $('.main-nav').addClass('sticky');
+        } else {
+            $('.main-nav').removeClass('sticky');
+
+        }
+    }, {
+        offset: '100px'
+    })
+});
